ce713fa3256dd19e802ae7018198b637c6a2141a
Your Name <gabriel.fial2005@gmail.com>
2026-08-31 15:55:55 +0100

    Incorporação do modelo LLAMA

--- SuportTeam/main.py
+++ SuportTeam/main.py
@@ -1,6 +1,10 @@
 # Estrutura principal para as funcionalidade da equipa de gestão que irão permitir responder aos clientes que não virem o seu
 # problema resolvido pelo chatAI
+import textwrap
 
 import ConectarBaseDados
+from ollama import generate
 
-ConectarBaseDados.get_database()
+#ConectarBaseDados.get_database()
+response = generate('llama3.2:1b','Quais as tuas principais funcionalidade?')
+print(textwrap.fill(response['response']))
